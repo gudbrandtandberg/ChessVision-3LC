c5e6bd87b21ed6a168065a9a3e1c069fa7ce9bda
Gudbrand Tandberg <gudbrand@3lc.ai>
2025-03-07 15:15:09 +0100

    Update VSCode launch configurations and web server settings

--- app/webroot/main.py
+++ app/webroot/main.py
@@ -1,35 +1,37 @@
 """
-The ChessVision developer webpage. 
+The ChessVision developer webpage.
 Allows upload to either a live instance, a local container, or a local webserver.
-Will not include any analysis, but will contain logic for editing results. 
+Will not include any analysis, but will contain logic for editing results.
 """
 
-from flask import Flask, render_template
 import argparse
 import sys
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 endpoint = None
 
-@app.route('/')
+
+@app.route("/")
 def home():
-  print(endpoint)
-  return render_template('index.html', endpoint=endpoint)
+    print(endpoint)
+    return render_template("index.html", endpoint=endpoint)
 
-if __name__ == '__main__':  
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--local", type=str)
-  args = parser.parse_args()
-  if not args.local:
-    endpoint = "lambda"
-  elif args.local == "container":
-    endpoint = "container"
-  elif args.local == "server":
-    endpoint = "server"
-  else:
-    print("--local flag must be either 'container' or 'server'")
-    sys.exit(1)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local", type=str)
+    args = parser.parse_args()
+    if not args.local:
+        endpoint = "lambda"
+    elif args.local == "container":
+        endpoint = "container"
+    elif args.local == "server":
+        endpoint = "server"
+    else:
+        print("--local flag must be either 'container' or 'server'")
+        sys.exit(1)
 
-  app.run(host='127.0.0.1', port=5000)
+    app.run(host="127.0.0.1", port=5000)
