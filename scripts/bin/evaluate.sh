959946720cb95bd406d1c1600e384d3e7a2b6010
S1M0N38 <bertolottosimone@gmail.com>
2025-10-01 17:17:32 +0200

    fix: add default weighth and update board-extractor-model-id default to None

--- scripts/bin/evaluate.sh
+++ scripts/bin/evaluate.sh
@@ -3,15 +3,15 @@
 # Simple wrapper for evaluate.py
 # Edit the arguments below as needed
 
+# --board-extractor-model-id "" mean that UNet is used
+
 python scripts/eval/evaluate.py \
     --image-folder "data/test/initial/raw" \
     --threshold 0.5 \
     --project-name "chessvision-testing" \
     --run-name "" \
     --run-description "" \
-    --board-extractor-weights "" \
-    --classifier-weights "" \
+    --board-extractor-weights "weights/best_extractor.pth" \
+    --classifier-weights "weights/best_yolo_classifier.pt" \
     --classifier-model-id "yolo" \
-    --include-metrics-table \
-    # --table-name "merged-2024-11-04-2024-11-04"
-    
+    --include-metrics-table
